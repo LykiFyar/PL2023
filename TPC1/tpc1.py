e27e60237a3bdb68750300ed26bd724a05314156
LykiFyar <joaopvbbraga@gmail.com>
2023-02-19 19:29:29 +0000

    tpc1 done

---
+++ TPC1/tpc1.py
@@ -0,0 +1,96 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# ler ficheiro
+def readfile(filename = "myheart.csv"):
+    file = open(filename)
+    file.readline()
+    
+    list = []
+    
+    for line in file:
+        list.append(line.strip().split(","))
+        
+    return list
+
+def distByGender(data):
+    
+    dict = {}
+    
+    for person in data:
+        if int(person[5]): # campo "temDoença"
+            
+           if person[1] not in dict: dict[person[1]] = 0
+           else: dict[person[1]] = dict[person[1]] + 1
+    
+    return dict
+    
+def distByAge(data):
+    
+    list = []
+    
+    for person in data:
+        if int(person[5]): # campo "temDoença"
+            index = int(int(person[0]) / 5)
+            while index >= len(list): 
+                list.append(0)
+            list[index] = list[index] + 1
+           
+    dict = {}
+    for index in range(len(list)):
+        if list[index] > 0:
+            dict["[" + str(index*5) + "-" + str(index*5+4) + "]"] = list[index]
+    
+    return dict
+
+def distByColesterolLevels(data):
+    
+    list = []
+    
+    for person in data:
+        if int(person[5]): # campo "temDoença"
+            index = int(int(person[3]) / 10)
+            while index >= len(list): 
+                list.append(0)
+            list[index] = list[index] + 1
+           
+    dict = {}
+    for index in range(len(list)):
+        if list[index] > 0:
+            dict["[" + str(index*10) + "-" + str(index*10+9) + "]"] = list[index]
+    
+    return dict
+
+def makeTable(dict, column1, column2):
+    print("====================")
+    print(f"{column1}     │ {column2}")
+    print("====================")
+    for key in dict:
+        print(f" {key}     {dict[key]} ")
+    print("====================\n")
+
+
+def printGraph(dict, tablelabel, xlabel, ylabel):
+
+    # plot
+    fig, ax = plt.subplots()
+
+    ax.bar(dict.keys(), dict.values(),align='center', edgecolor="white")
+
+    ax.set(xlim=(-1, len(dict.keys())), xticks=np.arange(0, len(dict.keys())), ylim=(0, len(dict.values())), yticks=np.arange(0, max(dict.values()), 10))
+    
+    plt.title(tablelabel)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.show()
+
+
+        
+
+data = readfile()
+makeTable(distByGender(data), "Sexo", "Número de Doentes")
+printGraph(distByGender(data), "Distribuição da Doença por Sexo","Sexo", "Número de Doentes")
+makeTable(distByAge(data), "Idade", "Número de Doentes")
+printGraph(distByAge(data), "Distribuição da Doença por Faixa Etária","Idade", "Número de Doentes")
+makeTable(distByColesterolLevels(data), "Nível de Colesterol", "Número de Doentes")
+printGraph(distByColesterolLevels(data), "Distribuição da Doença por Nível de Colesterol", "Nível de Colesterol", "Número de Doentes")
