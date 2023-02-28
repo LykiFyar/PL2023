830e83385274528c157c076135dc11d44e52a17e
LykiFyar <joaopvbbraga@gmail.com>
2023-02-28 22:25:31 +0000

    tpc2 feito

--- TPC2/tpc2.py
+++ TPC2/tpc2.py
@@ -1,5 +1,40 @@
+from sys import stdin
+
 def main():
-    while 1:
-        input = input("Trabalhe:")
-        while input != "":
-            print()
+    on = False
+    totalsum = 0
+    
+    for line in stdin:
+        line = line.lower()
+        i = 0
+        
+        while i < len(line):
+            
+            if(line[i] == '='):
+                print(f"A soma atual é: {totalsum}")
+                i += 1
+                
+            else:
+                if(on):
+                    if(line[i].isdigit()):
+                            
+                        j = 1
+                        
+                        while(i+j < len(line) and line[i+j].isdigit()):
+                            j += 1
+                        
+                        totalsum += int(line[i:i+j])
+                        i += j
+                
+                if(line[i:i+2] == 'on'):
+                    on = True
+                    i += 2
+                
+                elif(line[i:i+3] == 'off'):
+                    on = False
+                    i += 3
+                                
+                else:
+                    i += 1
+
+main()
