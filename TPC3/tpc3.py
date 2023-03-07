ef7037cfd94c77ae5907810f814d9d510235f7c4
LykiFyar <joaopvbbraga@gmail.com>
2023-03-07 23:48:22 +0000

    TPC3 Feito

--- TPC3/tpc3.py
+++ TPC3/tpc3.py
@@ -1,5 +1,6 @@
 import re
 import matplotlib.pyplot as plt
+import json
 
 
 def loadFile(path):
@@ -9,12 +10,14 @@ def loadFile(path):
         for line in file.readlines():
             reg = re.match(r"(?P<número>\d*)::(?P<data>\d{4}-\d{2}-\d{2})::(?P<nome>[^:]*)::(?P<pai>[^:]*)::(?P<mãe>[^:]*)::(?P<info>[^:]*)::\s*",line)
             if (reg is not None):
-                array.append(reg.groupdict())
-    
+                res = reg.groupdict()
+                if (res['info'] is not None):
+                    res['info'] = re.split(r"\s*\.\s*",res['info'])
+                array.append(res)
     return array
     
 
-
+# a. Calcula a frequência de processos por ano (primeiro elemento da data);
 def calcFreqRegistosAno(array):
     dict = {}
     
@@ -31,14 +34,13 @@ def calcFreqRegistosAno(array):
     return dict
 
 def calcFreqNomes(array):
-    nomesP,apelidos,top5 = {},{},{}
+    nomesP,apelidos = {},{}
     
-    regex = re.compile(r"^(?P<nomeP>[^\w]*).*\s*(?P<apelido>[^\w]*)$")
+    regex = re.compile(r"^(?P<nomeP>\w*).*\s(?P<apelido>\w*)$")
     
     for line in array:
         if(line['nome'] is not None):
             nome = re.match(regex, line['nome'])
-            print(str(nome.groupdict()))
             if nome is not None:
                 nomeP = nome['nomeP']
                 apelido = nome['apelido']
@@ -51,8 +53,53 @@ def calcFreqNomes(array):
                         apelidos[apelido] = 0
                     apelidos[apelido] = apelidos[apelido] + 1
         
-    return nomesP,apelidos,top5
+    return nomesP,apelidos
 
+def calcFreqRelacoes(array):
+    rels = {}
+    
+    regex = re.compile(r".*\,\s*(?P<relacao>\w*).*$")
+    
+    relacoes_validas = ["avo", "avos", "mae", "pai", "pais", "irmao", "irmaos", "tio", "tios", "neto", "netos", "filho", "filhos", "bisavo", "bisavos", "primo", "primos", "sobrinho", "sobrinhos"]
+    
+    for line in array:
+        if(line['info'] is not None):
+            for inforegistry in line['info']:
+                relacao = re.match(regex, inforegistry.lower())
+                if relacao is not None:
+                    rel = relacao.group(1)
+                    if rel is not None:
+                        if rel in relacoes_validas:
+                            if rels.get(rel) is None:
+                                rels[rel] = 0
+                            rels[rel] = rels[rel] + 1
+    return rels
+
+def convert_to_json(array):
+    dict = {}
+    
+    dict['pessoas'] = array
+    
+    with open("pessoas.json", "w", encoding="utf-8") as file:
+        json_str = json.dumps(dict)
+        file.write(json_str)
+
+
+def makeTableByDict(dict, column1, column2):
+    print("====================")
+    print(f"{column1}     │ {column2}")
+    print("====================")
+    for key in dict:
+        print(f" {key}     {dict[key]} ")
+    print("====================\n")
+    
+def makeTableByTupleList(list, column1, column2):
+    print("====================")
+    print(f"{column1}     │ {column2}")
+    print("====================")
+    for item,value in list:
+        print(f" {item}     {value} ")
+    print("====================\n")
 
 def printGraph(dict, tablelabel, xlabel, ylabel):
 
@@ -66,8 +113,43 @@ def printGraph(dict, tablelabel, xlabel, ylabel):
 
 
 array = loadFile("processos.txt")
-#printGraph(calcFreqRegistosAno(array), "Frequência de processos por ano","Ano", "Frequência")
-#printGraph(calcFreqNomes(array)[0], "Frequência de nomes próprios por ano","Nome Próprio", "Frequência")
-
-
-print(calcFreqNomes(array)[0])
+opcao = -1
+while opcao!=0:
+    
+    print("1. Gráfico de frequência de processos por ano")
+    print("2. Gráfico de frequência de nomes próprios por ano")
+    print("3. Gráfico de frequência de apelidos por ano")
+    print("4. Top 5 Nomes Próprios")
+    print("5. Top 5 Apelidos")
+    print("6. Frequência de vários tipos de relações de parentesco")
+    print("7. Criar ficheiro Json com primeiros 20 registos\n")
+    opcao = int(input("Indique a opção: "))
+    
+    # a. Calcula a frequência de processos por ano (primeiro elemento da data)
+    if opcao==1:
+        printGraph(calcFreqRegistosAno(array), "Frequência de processos por ano","Ano", "Frequência")
+    
+    # b. Calcula a frequência de nomes próprios (o primeiro em cada nome) e apelidos (o ultimo em cada nome) por séculos
+    elif opcao==2:
+        printGraph(calcFreqNomes(array)[0], "Frequência de nomes próprios por ano","Nome Próprio", "Frequência")
+    elif opcao==3:
+        printGraph(calcFreqNomes(array)[1], "Frequência de Apelidos por ano","Apelido", "Frequência")
+    elif opcao==4:
+        nomesP,apelidos = calcFreqNomes(array)
+        print(f"Top 5 Nomes Próprios Usados: ")
+        makeTableByTupleList(sorted(nomesP.items(), key=lambda x:x[1], reverse=True)[:5], "Nomes Próprios", "Frequência")
+    elif opcao==5:
+        nomesP,apelidos = calcFreqNomes(array)
+        print(f"Top 5 Apelidos Usados: ")
+        makeTableByTupleList(sorted(apelidos.items(), key=lambda x:x[1], reverse=True)[:5], "Apelidos", "Frequência")
+    
+    # c. Calcula a frequência dos vários tipos de relação: irmão, sobrinho, etc.
+    elif opcao==6:
+        rels = calcFreqRelacoes(array)
+        print(rels)
+        print(f"Frequência de vários tipos de relação: ")
+        printGraph(rels, "Frequência de relações de parentesco", "Relações de Parentesco", "Frequência")
+    # d. Converta os 20 primeiros registos num novo ficheiro de output mas em formato Json.
+    elif opcao==7:
+        print("\n\n\n")
+        convert_to_json(array[:20])
